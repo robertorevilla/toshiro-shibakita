c45a71d3270838a73081f14b6f417c13bafd486f
Roberto Revilla <64987740+robertorevilla@users.noreply.github.com>
2022-08-28 18:41:04 -0300

    Update banco.sql
    Tabela produtos supermercado Shibakita.

--- banco.sql
+++ banco.sql
@@ -1,8 +1,8 @@
 CREATE TABLE dados (
-    AlunoID int,
-    Nome varchar(50),
-    Sobrenome varchar(50),
-    Endereco varchar(150),
-    Cidade varchar(50),
-    Host varchar(50)
+    ProdutoID int,
+    Codigo int,
+    Produto varchar(50),
+    Quantidade int,
+    Preco int,
+    Total int
 );
